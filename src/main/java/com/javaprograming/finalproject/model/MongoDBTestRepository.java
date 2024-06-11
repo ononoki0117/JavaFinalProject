3b96a997991f67dbb8b2ddc30f7f0c0d49c9e4d3
ononoki0117 <111170868+ononoki0117@users.noreply.github.com>
2024-06-11 13:48:14 +0900

    test code deleted

--- src/main/java/com/javaprograming/finalproject/model/MongoDBTestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javaprograming.finalproject.model;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MongoDBTestRepository extends MongoRepository<MongoDBTestModel, String> {
-    MongoDBTestModel findByName(String name);
-}
