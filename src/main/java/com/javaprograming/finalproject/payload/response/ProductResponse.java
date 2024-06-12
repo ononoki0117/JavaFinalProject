a217846166117e9daa4a98348c5970830bf07d06
ononoki0117 <tytttyty@naver.com>
2024-06-12 18:20:29 +0900

    제품들까지 db에 올림

---
+++ src/main/java/com/javaprograming/finalproject/payload/response/ProductResponse.java
@@ -0,0 +1,19 @@
+package com.javaprograming.finalproject.payload.response;
+
+import com.javaprograming.finalproject.models.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductResponse extends Product {
+    private String link;
+    public ProductResponse(Product product) {
+        this.setId(product.getId());
+        this.setName(product.getName());
+        this.setDescription(product.getDescription());
+        this.setPrice(product.getPrice());
+        this.setImage(product.getImage());
+        this.link = "/products/" + product.getId();
+    }
+}
